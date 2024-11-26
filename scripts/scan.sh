3187f2d9f0446e2c57e1d6c3804eb4aefdaf653e
DavidC <david.dch21@gmail.com>
2024-11-26 13:05:42 +1100

    K: credits

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,7 @@
-# TODO: write the script
+echo "\n===== SCANNING FOR CREDIT CARD NUMBERS =====\n"
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git}
+echo "==== CREIT CARD SCAN COMPLETE ===="
+
+echo "\n===== SCANNING FOR SECURITY NUMBERS =====\n"
+grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "==== SOCIAL SECURITY SCAN COMPLETE ===="
